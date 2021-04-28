e3513fe4eb3ab86323b470f5658f14b23e03fd4c
memacs <boyofmonk@gmail.com>
2021-04-28 13:19:08 +0800

    修改模板

--- init.el
+++ init.el
@@ -19,9 +19,43 @@
 (setq-default pathname-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 
+
+;;(require 'yasnippet)
+;;(yas-global-mode 1)
+;; go get golang.org/x/tools/gopls@latest
+;; https://studygolang.com/articles/28702?fr=sidebar
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; Optional - provides fancier overlays.
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+;; Company mode is a standard completion package that works well with lsp-mode.
+(use-package company
+  :ensure t
+  :config
+  ;; Optionally enable completion-as-you-type behavior.
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
+
 ;; http://joaotavora.github.io/yasnippet/
-(require 'yasnippet)
-(yas-global-mode 1)
+;; Optional - provides snippet support.
+(use-package yasnippet
+  :ensure t
+  :commands yas-minor-mode
+  :hook (go-mode . yas-minor-mode))
+
 
 ;; Auto add HEADER in new file
 (add-hook 'find-file-hook
